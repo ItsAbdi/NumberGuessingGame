e8ef47be4be710c126ec3e36425d889148ee81fb
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-10-04 14:12:10 -0400

    tested

---
+++ src/NumberGuessingGame2.java
@@ -0,0 +1,7 @@
+public class NumberGuessingGame2 {
+    public static void main(String[] args){
+        int pickedNumber;
+        pickedNumber= (int) (Math.random()*100);
+        System.out.println(pickedNumber);
+    }
+}
