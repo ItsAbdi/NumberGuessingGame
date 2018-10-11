ac6ef4d2a053530d3f1f55be7f876f21c1c2467a
Abdi Sirat <abdikadar.sirat.2021@student.lhschools.org>
2018-10-11 13:21:27 -0400

    added a play again method to the Number Guessing Game

--- src/NumberGuessingGame2.java
+++ src/NumberGuessingGame2.java
@@ -2,49 +2,66 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumberGuessingGame2 {
+
+
     public static void main(String[] args){
 //        int pickedNumber;
 //        pickedNumber= (int) (Math.random()*100);
 //        System.out.println(pickedNumber);
 
+       Scanner keyboard;
+       String again;
 
-        System.out.println("Hello and welcome to the Number Guessing Game.");
-        System.out.println("Pick a number: ");
+       keyboard= new Scanner(System.in);
 
-        Scanner inputnum = new Scanner(System.in);
-        int maxnum;
-        maxnum = inputnum.nextInt();
 
-        Random rand = new Random();
-        int number = rand.nextInt(maxnum);
-        int tries = 0;
-        Scanner input = new Scanner(System.in);
-        int guess;
-        boolean win = false;
 
-        while (win == false){
+       do {
+           System.out.println("Hello and welcome to the Number Guessing Game.");
+           System.out.println("Pick a number: ");
 
-            System.out.println("Guess a number between 1 and "+ maxnum +": ");
-            guess = input.nextInt();
-            tries++;
+           Scanner inputnum = new Scanner(System.in);
+           int maxnum;
+           maxnum = inputnum.nextInt();
 
-            if (guess == number){
-                win = true;
-            }
 
-            else if(guess < number){
-                System.out.println("Number is to low, try again");
+           Random rand = new Random();
+           int number = rand.nextInt(maxnum);
+           int tries = 0;
+           Scanner input = new Scanner(System.in);
+           int guess;
+           boolean win = false;
 
-            }
+           while (win == false) {
 
-            else if(guess > number){
-                System.out.println("Number is to high, try again");
+               System.out.println("Guess a number between 1 and " + maxnum + ": ");
+               guess = input.nextInt();
+               tries++;
 
-            }
+               if (guess == number) {
+                   win = true;
+               } else if (guess < number) {
+                   System.out.println("Number is to low, try again");
 
-        }
+               } else if (guess > number) {
+                   System.out.println("Number is to high, try again");
 
-        System.out.println("You win!");
-        System.out.println("It took you "+ tries + " tries.");
+               }
+
+           }
+
+           System.out.println("You win!");
+           System.out.println("It took you " + tries + " tries.");
+
+
+           System.out.println("would you like to play again (yes or no).");
+           again = keyboard.next();
+
+       } while (again.equals("yes"));
+            System.out.println("Goodbye");
+
+
+
+
     }
 }
